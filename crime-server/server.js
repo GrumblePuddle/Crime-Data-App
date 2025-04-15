bfe8eba216b991f9427b0045d01812e7780f055f
Gustav Liffner <gurra_liffner@hotmail.com>
2025-04-15 19:24:31 +0100

    All routing completed

--- crime-server/server.js
+++ crime-server/server.js
@@ -1,32 +1,73 @@
 import express from "express";
-import swagger from "swagger-ui-express";
-import swaggerJsDoc from "swagger-jsdoc";
-
-app.use(express.json());
+import axios from "axios";
 
 const app = express();
 const PORT = 3000;
 
-const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Crime API",
-      version: "1.0.0",
-      description: "API for managing crime data",
-    },
-    servers: [
-      {
-        url: `http://localhost:${PORT}`,
-      },
-    ],
-  },
-  apis: ["./server.js"],
-};
+app.use(express.json());
 
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.get("/crimes", async (req, res, next) => {
+  try {
+    const response = await axios.get(
+      `https://brottsplatskartan.se/api/events/?location=vastervik&limit=5`
+    );
+    res.json(response.data.data);
+    console.log(response.data.data);
+  } catch (err) {
+    console.error(err);
+    next(err); // Pass the error to the error handling middleware
+  }
+});
 
-app.get("/crimes", (req, res) => {});
+app.get("/crimes/locations", async (req, res, next) => {
+  try {
+    const response = await axios.get(
+      `https://brottsplatskartan.se/api/locations/?location=vastervik&limit=5`
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    res.json(response.data.data.map((item) => item.headline));
+    console.log(response.data.data.map((item) => item.headline));
+  } catch (err) {
+    console.error(err);
+    next(err); // Pass the error to the error handling middleware
+  }
+});
+
+app.get("/crimes/search", async (req, res, next) => {
+  const location = req.query.city || "vastervik";
+  try {
+    const response = await axios.get(
+      `https://brottsplatskartan.se/api/events/?location=${location}&limit=5`
+    );
+    res.json(response.data.data);
+    console.log(response.data.data);
+  } catch (err) {
+    console.error(err);
+    next(err); // Pass the error to the error handling middleware
+  }
+});
+
+app.get("/crimes/latest", async (req, res, next) => {
+  try {
+    const response = await axios.get(
+      `https://brottsplatskartan.se/api/events/?location=vastervik&limit=5`
+    );
+    res.json(response.data.data[0]);
+    console.log(response.data.data[0]);
+  } catch (err) {
+    console.error(err);
+    next(err); // Pass the error to the error handling middleware
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    error: err.message || "Internal Server Error",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
